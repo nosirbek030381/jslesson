b1c097051582cb59201ba3f2f5a0fa951e4a4768
nosirbek030381 <ismoilovn899@gmail.com>
2023-04-22 11:29:09 +0500

    vazifa6

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 "use strict";
 
-const adv = document.querySelectorAll('.promo__adv img'),
+document.addEventListener('DOMContentLoaded', () => {
+    const adv = document.querySelectorAll('.promo__adv img'),
     wrapper = document.querySelector('.promo__bg'),
     genre = wrapper.querySelector('.promo__genre'),
-    seriesList = document.querySelector('.promo__interactive-list');
+    seriesList = document.querySelector('.promo__interactive-list'),
+    addForm = document.querySelector('form.add'),
+    inputVal = addForm.querySelector('.adding__input'),
+    checkbox = addForm.querySelector('[type="checkbox"]');
 
 
 const seriesDB = {
@@ -16,21 +20,74 @@ const seriesDB = {
     ],
 };
 
-adv.forEach(item => {
-    item.remove();
-});
+addForm.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-genre.textContent = 'comedy';
+    let newSeries = inputVal.value;
+    const favorite = checkbox.checked;
 
-wrapper.style.backgroundImage = 'url("img/1.jpg")';
+    if(newSeries) {
+        if(newSeries.length > 18) {
+            newSeries = `${newSeries.substring(0, 18)}...`;
+        } 
+        if(favorite) {
+            console.log("Sevimli serial qo'shildi");
+        }
+       seriesDB.series.push(newSeries)
+       sortArr(seriesDB.series)
+    
+        createSeriesList(seriesDB.series, seriesList) 
+    }
+
+    
+
+    event.target.reset()
+})
+
+const deleteAdv = (arr) => {
+    adv.forEach(item => {
+        item.remove();
+    });
+}
+
+const makeChanges = () => {
+    genre.textContent = 'comedy';
+
+    wrapper.style.backgroundImage = 'url("img/1.jpg")';
+}
+
+const sortArr = (arr) => {
+    arr.sort()
+}
 
 
-seriesList.innerHTML ='';
+function createSeriesList(series, parent) {
+    parent.innerHTML ='';
+    sortArr(series)
 
-seriesDB.series.forEach((item, idn ) => {
-    seriesList.innerHTML +=`
+    series.forEach((item, idn ) => {
+        parent.innerHTML +=`
             <li class="promo__interactive-item">${idn + 1} ${item}
                 <div class="delete"></div>
             </li>
     ` ;
 });
+
+document.querySelectorAll('.delete').forEach((trash, idx) => {
+    trash.addEventListener('click', () => {
+        trash.parentElement.remove()
+        seriesDB.series.splice(idx, 1)
+
+        createSeriesList(series, parent)
+    })
+})
+
+}
+
+sortArr(seriesDB.series)
+deleteAdv(adv)
+makeChanges()
+createSeriesList(seriesDB.series, seriesList)
+
+})
+
